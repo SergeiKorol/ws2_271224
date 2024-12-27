6a59adc097ec38e01bcd151a7e52c8d197b83099
qsmitova <qsmitova@gmail.com>
2024-12-27 21:23:02 +0400

    Тест о выполнении задачи

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -4,6 +4,7 @@ def test_add():
     body = {"title":"generated","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
+    pass
     
     assert response.status_code == 201
     assert response_body['completed'] == False
