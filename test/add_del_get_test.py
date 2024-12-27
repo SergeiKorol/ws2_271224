682e0fd5b92bd5430d333b80c823e2ff4956228a
gruzlit <gruzlit@gmail.com>
2024-12-28 00:34:49 +0700

    тест про удаление

---
+++ test/add_del_get_test.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_add_del_get():
+    body = {"title": "generated", "completed": False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+
+    requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
+
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+
+    assert response.status_code == 404
+
